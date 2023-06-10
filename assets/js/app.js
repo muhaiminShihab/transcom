fd6af8aa2ef7c2f5175e19ad9e83858f4163ec91
Muhaimin Shihab <talk.shihab@gmail.com>
2023-06-11 00:28:03 +0600

    update login input

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+// login page (input with country code)
+const phoneInputField = document.querySelector("#phone");
+const phoneInput = window.intlTelInput(phoneInputField, {
+    utilsScript:
+        "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
+});
