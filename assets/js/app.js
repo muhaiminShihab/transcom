22cf7463b8ff96ff33980013896aa099144e2cc9
Muhaimin Shihab <talk.shihab@gmail.com>
2023-06-18 14:36:56 +0600

    add login modal

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,39 +1,44 @@
-// home page (category slider)
-$('.category-slider').slick({
-    infinite: true,
-    // autoplay: true,
-    // autoplaySpeed: 1500,
-    speed: 1500,
-    prevArrow: "<button class='prevdot'><i class='fas fa-angle-left'></i></button>",
-    nextArrow: "<button class='nextdot'><i class='fas fa-angle-right'></i></button>",
-    slidesToShow: 6,
-    slidesToScroll: 1,
-    responsive: [
-        {
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
-            }
-        },
-        {
-            breakpoint: 600,
-            settings: {
-                slidesToShow: 2,
-            }
-        },
-        {
-            breakpoint: 480,
-            settings: {
-                slidesToShow: 1,
-            }
-        }
-    ]
-});
-
 // login page (input with country code)
 const phoneInputField = document.querySelector("#phone");
-const phoneInput = window.intlTelInput(phoneInputField, {
-    utilsScript:
-        "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
+
+if (phoneInputField != undefined || phoneInputField != null) {
+    const phoneInput = window.intlTelInput(phoneInputField, {
+        utilsScript:
+            "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
+    });
+}
+
+$(document).ready(function () {
+    // home page (category slider)
+    $('.category-slider').slick({
+        infinite: true,
+        // autoplay: true,
+        // autoplaySpeed: 1500,
+        speed: 1500,
+        prevArrow: "<button class='prevdot'><i class='fas fa-angle-left'></i></button>",
+        nextArrow: "<button class='nextdot'><i class='fas fa-angle-right'></i></button>",
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+        ]
+    });
 });
